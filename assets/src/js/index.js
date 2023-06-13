46034bd6a8525a08cb0e4503e7360521bca470fa
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-06-12 21:07:07 -0300

    :dizzy:colocando o JS para o swiper das fotos e a animação do pop-up

---
+++ assets/src/js/index.js
@@ -0,0 +1,98 @@
+document.addEventListener('DOMContentLoaded', () => {
+    for(let btn of document.querySelectorAll('[data-target]')) {
+        btn.addEventListener('click', (e) => {
+            if(document.querySelector(btn.dataset.target).dataset.state == 'false') {
+                document.querySelector(btn.dataset.target).dataset.state = 'true';
+                if( btn.querySelector('.fa-caret-down')) {
+                    btn.querySelector('.fa-caret-down').classList.add('fa-caret-up');
+                    btn.querySelector('.fa-caret-down').classList.remove('fa-caret-down');
+                }
+            } else {
+                document.querySelector(btn.dataset.target).dataset.state = 'false';
+                if( btn.querySelector('.fa-caret-up')) {
+                    btn.querySelector('.fa-caret-up').classList.add('fa-caret-down');
+                    btn.querySelector('.fa-caret-up').classList.remove('fa-caret-up');
+                }
+            }
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    var servicesSwiper = new Swiper('.services-swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+    
+         slidesPerView: 1,
+         spaceBetween: 10,
+        // slidesPerView: 'auto',
+        // slidesPerGroup: 1,
+        // sliderPerGroupAuto: true,
+
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is >= 768px
+            426: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
+    });
+
+    var produtosSwiper = new Swiper('.produtos-swiper', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 2,
+        spaceBetween: 10,
+        breakpoints: {
+            // when window width is >= 768px
+            426: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            },
+            1440: {
+                slidesPerView: 6,
+                spaceBetween: 30
+            }
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+      });
+
+      var servicesSelectionSwiper = new Swiper('.services-selection-swiper', {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 1,
+        spaceBetween: 10,
+        breakpoints: {
+            // when window width is >= 768px
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        }
+      });
+});
